c09f78d8036bc3bf4e32ac5ab217502e70faaa97
denandserg <denandserg@gmail.com>
2018-12-13 01:07:30 +0200

    Xamarin Google Maps
    Signed-off-by: denandserg <denandserg@gmail.com>

---
+++ XamarinGMaps/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Gms.Maps;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace XamarinGMaps
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, IOnMapReadyCallback
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.activity_main);
+
+            var mapFragment = (MapFragment)FragmentManager.FindFragmentById(Resource.Id.map);
+            mapFragment.GetMapAsync(this);
+
+            
+        }
+
+        public void OnMapReady(GoogleMap map)
+        {
+            map.MapType = GoogleMap.MapTypeHybrid;
+            map.UiSettings.ZoomControlsEnabled = true;
+            map.UiSettings.CompassEnabled = true;
+        }
+    }
+}
